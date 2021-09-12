291016ca8ff25a3b25041c0c4099e1b2abc2a0ae
impact-eintr <yixingwei4@gmail.com>
2021-09-12 20:01:53 +0800

    学习一下spring

---
+++ src/main/java/com/eintr/firewarn/FirewarnApplication.java
@@ -0,0 +1,13 @@
+package com.eintr.firewarn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirewarnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirewarnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eintr/firewarn/FirewarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eintr.firewarn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirewarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
