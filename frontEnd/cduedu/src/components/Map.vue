d9775ce74512173767a22678ecf1d4449325097d
张昊 <zhanghao@1-PC68.lab.hwadee.com>
2021-09-16 17:39:12 +0800

    后台地图

---
+++ frontEnd/cduedu/src/components/Map.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>地图查询</el-breadcrumb-item>
+    </el-breadcrumb>
+      <iframe src="http://localhost:9427/edu"  class="map"></iframe>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "Map"
+}
+</script>
+
+<style scoped>
+
+.container {
+  height: 100%;
+}
+
+.map {
+  margin: 1em 0 0 0;
+  height: 95%;
+  width: 100%;
+  border: #688383 dashed 1px;
+}
+</style>
---
+++ frontEnd/cduedu/src/components/Org.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>组织机构</el-breadcrumb-item>
+      <el-breadcrumb-item>组织列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="card">
+      <el-table></el-table>
+    </el-card>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Org"
+}
+</script>
+
+<style scoped>
+
+.container {
+  height: 100%;
+}
+
+.card {
+  margin: 1em 0 0 0;
+  box-shadow: -1px 1px 20px grey;
+  height: 95%;
+}
+</style>
